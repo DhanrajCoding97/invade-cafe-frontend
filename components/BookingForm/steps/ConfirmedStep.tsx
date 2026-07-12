d9db582f8a673a4b78084f912fb8e98dbab57aca
DhanrajCoding97 <dhanrajcoding@gmail.com>
2026-07-13 02:36:23 +0530

    added steps skeleton

---
+++ components/BookingForm/steps/ConfirmedStep.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ConfirmedStep() {
+  return (
+    <div>ConfirmedStep</div>
+  )
+}
---
+++ components/BookingForm/steps/DurationStep.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DurationStep() {
+  return (
+    <div>DurationStep</div>
+  )
+}
